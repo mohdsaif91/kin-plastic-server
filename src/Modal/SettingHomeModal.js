c21dc55eb48819bcb848e9275203fd676f499aca
MohdSaif <MohdSaif@nextgen.com>
2021-10-21 23:19:50 +0530

    many-changes

--- src/Modal/SettingHomeModal.js
+++ src/Modal/SettingHomeModal.js
@@ -1,12 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const settingHomeSchema = mongoose.Schema({
-	homeHeroColor: {
-		require: true,
-		type: String,
-	},
+  homeHeroColor: {
+    require: true,
+    type: String,
+  },
+  paralexData: [],
+  card1: {
+    heading: {
+      type: String,
+    },
+    subHeading: {
+      type: String,
+    },
+  },
+  card2: {
+    heading: {
+      type: String,
+    },
+    subHeading: {
+      type: String,
+    },
+  },
+  card3: {
+    heading: {
+      type: String,
+    },
+    subHeading: {
+      type: String,
+    },
+  },
+  quotes: {
+    type: String,
+  },
+  videoCode: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
 });
 
-const settingHomeModal = mongoose.model('setting', settingHomeSchema);
+const settingHomeModal = mongoose.model("setting", settingHomeSchema);
 
 module.exports = settingHomeModal;
